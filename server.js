ada737190198ee390b1672a90b37168682ef5b23
dudeicejedi <dudeicejedi@gmail.com>
2026-09-25 14:10:48 +0200

    Enable trust proxy setting in Express app

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import { createClient } from '@supabase/supabase-js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
